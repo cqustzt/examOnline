7a358671b61593e8b836172d0b8049f72ad91951
zt <674342042@qq.com>
2018-12-10 22:31:45 +0800

    测试提交1111

--- src/com/aop/AspectAdvice2.java
+++ src/com/aop/AspectAdvice2.java
@@ -3,9 +3,9 @@ package com.aop;
 public class AspectAdvice2 {
 	
 	public void after() throws Exception{
-        System.out.println("=================AOP SUCCESS==================");
+        System.out.println("============AOP SUCCESS==================");
 	}
 	public void after1() throws Exception{
-        System.out.println("================AOP SUCCESS1==================");
+        System.out.println("===========AOP SUCCESS1==================");
 	}
 }
